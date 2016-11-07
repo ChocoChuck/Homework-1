e8681ab9517742acc42f3cc58efe70e1ec0edd96
Chuck Oguagha <coguagha1@gmail.com>
2016-11-07 18:36:08 -0500

    Correct homework

--- homework1.rb
+++ homework1.rb
@@ -1,20 +1,25 @@
 # creates a function
 def full_name(first_name, last_name, title)
+  #this equates the variable "name" to have no value
   name = nil
-# creates an argument
+# creates an argument for if they use all three parameters
   if title && first_name && last_name
+    #makes the variable "name" to equal the concatenation of the three parameters
     name = title + ' ' + first_name + ' ' + last_name
-    # creates another option for that argument
+    # creates another option for if they only put in a title and a last name
   elsif title && last_name
+    #makes the variable "name" to equal to the concatenation of "title", "a space" and "last_name"
     name = title + ' ' + last_name
     # creates another option for that argument
   elsif first_name && last_name
+    #makes the variable "name" to equal to the concatenation of "first_name", "a space" and "last_name"
     name = first_name + ' ' + last_name
     # creates another option for that argument
   elsif first_name
     name = first_name
     # final choice if all eles fails
   else
+    #alerts the user with that quote
     raise "That doesn't look like a name!"
   end
   # returns the variable
@@ -22,15 +27,13 @@ def full_name(first_name, last_name, title)
 end
 
 def add(x, y)
-  z = x + y
-  puts z
+  x + y
 end
 
 puts add(1,3)
 
 def join_strings(a, b)
-  word = a + b
-  puts word
+  a + b
 end
 
 puts join_strings('Hello', " Dolly!")
